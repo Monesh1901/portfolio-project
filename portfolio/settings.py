5af9fd7e7a663620d61197cfaab907fb87249ecf
Monesh1901 <saimonesh007@gmail.com>
2021-07-18 16:03:51 +0530

    Jobs app up and running

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': 'monesh123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -112,7 +116,8 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
+
 
 
 # Static files (CSS, JavaScript, Images)
